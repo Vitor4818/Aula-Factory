cfcf5ab10849e51bb16c19336b887749289792f4
Vitor Gomes <vitorg.martins4818@gmail.com>
2024-09-20 20:25:01 -0300

    Organizando projeto

---
+++ src/factory/Poligono.java
@@ -0,0 +1,6 @@
+package factory;
+
+public interface Poligono {
+    public int getNumeroLados();
+
+}
---
+++ src/factory/Triangulos.java
@@ -0,0 +1,12 @@
+package factory;
+
+public class Triangulos implements Poligono{
+
+
+    @Override
+    public int getNumeroLados() {
+        return 3;
+    }
+
+
+}
