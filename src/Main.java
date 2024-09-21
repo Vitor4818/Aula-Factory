e8cb59bc3c4b936d68b576a4845263dae77de21e
Vitor Gomes <vitorg.martins4818@gmail.com>
2024-09-20 22:33:40 -0300

    Arrumando

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(PoligonoFactory.getPoligono(0));
         System.out.println(PoligonoFactory.getPoligono(3));
-    }asdfasfd
+    }
 }
